46205e4f9a90541dd9af2a0a2cbbc970fdab2aec
Jayesh <jayeshpawar.1312@gmail.com>
2024-09-13 20:13:28 +0530

    reverted base url to localhost

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = (phase) => {
         mongodb_password: "lQZwniM4j3QhemVD",
         mongodb_clustername: "cluster0",
         mongodb_database: "shopping-app",
-        BASE_URL: window.location.origin,
+        BASE_URL: "http://localhost:3000",
       },
     };
   }
@@ -18,7 +18,7 @@ module.exports = (phase) => {
       mongodb_password: "lQZwniM4j3QhemVD",
       mongodb_clustername: "cluster0",
       mongodb_database: "shopping-app",
-      BASE_URL: window.location.origin,
+      BASE_URL: "http://localhost:3000",
     },
   };
 };
